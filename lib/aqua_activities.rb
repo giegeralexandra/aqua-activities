f0070e1979f0ed0b114a3dd3b560eeb53f73e0e3
giegeralexandra <giegeralexandra@gmail.com>
2020-09-10 21:04:23 -0400

    loaded gem and put files into gem

---
+++ lib/aqua_activities.rb
@@ -0,0 +1,6 @@
+require "aqua_activities/version"
+
+module AquaActivities
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/aqua_activities_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe AquaActivities do
+  it "has a version number" do
+    expect(AquaActivities::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
